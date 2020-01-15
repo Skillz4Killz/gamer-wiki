af09e65820b5128362ef659c6c0b1d34989e821a
Skillz <gankstarsleague@gmail.com>
2020-01-15 09:24:04 -0500

    small fixes

--- src/@lekoarts/gatsby-theme-emma/components/note.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/note.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./note.css";
+import { Header as ThemeHeader, jsx, useColorMode, Styled } from "theme-ui";
 
 export default props => {
+  const [colorMode, setColorMode] = useColorMode();
+  const isDark = colorMode === `dark`;
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`);
+  };
+
   if (props.type === `note`)
     return (
-      <div className={`alert alert-${props.type}`}>
+      <div className={`alert alert-${props.type} ${colorMode}Theme`}>
         <b>Note: </b>
         {props.children}
       </div>
     );
 
-  if (props.type === `command`) return <div className={`alert alert-${props.type}`}>{props.children}</div>;
+  if (props.type === `command`) return <div className={`alert alert-${props.type} ${colorMode}Theme`}>{props.children}</div>;
   if (props.type === `advanced`)
     return (
-      <div className={`alert alert-${props.type}`}>
+      <div className={`alert alert-${props.type} ${colorMode}Theme`}>
         <b>Advanced: </b>
         {props.children}
       </div>
